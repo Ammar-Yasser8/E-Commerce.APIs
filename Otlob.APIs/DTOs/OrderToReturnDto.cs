993aedcc42e3c3b3aeff85b2fd2648e74822dc99
ammar892001@outlook.com <ammar.yasser8920@gmail.com>
2025-02-16 23:24:50 +0200

    Test payment cycle

--- Otlob.APIs/DTOs/OrderToReturnDto.cs
+++ Otlob.APIs/DTOs/OrderToReturnDto.cs
@@ -15,6 +15,6 @@ namespace Otlob.APIs.DTOs
         public ICollection<OrderItemDto> Items { get; set; } = new HashSet<OrderItemDto>();
         public decimal SubTotal { get; set; }
         public decimal Total { get; set; }
-        public string PaymentIntentId { get; set; } = string.Empty;
+        public string PaymentIntentId { get; set; } 
     }
 }
